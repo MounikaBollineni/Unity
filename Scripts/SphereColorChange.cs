a09ef368fa24c5b1947edf521d04be5aa3f47859
Mounika Bollineni <saimounika417@gmail.com>
2022-09-17 10:56:06 +0530

    designs a color palate.

--- Scripts/SphereColorChange.cs
+++ Scripts/SphereColorChange.cs
@@ -1,27 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SphereColorChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
+    [SerializeField] private Material sphereMaterial;
 
-    // Update is called once per frame
-     void Update(){
-        if (Input.GetMouseButtonUp(0)) {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-
-            int layermask=1<<8;
-            layermask=~layermask;
-            RaycastHit2D hit = Physics2D.Raycast(transform.position,Input.mousePosition,1f,layermask);
-            Debug.Log(hit.transform.name);
-            Debug.Log(hit.collider.name);
-             //RaycastHit2D hit = Physics2D.Raycast(this.gameObject.transform.position, direction);
-           
-            Debug.DrawLine(transform.position,Input.mousePosition, hit.collider.GetComponent<RawImage>().color,1f); 
+    void Start(){
+        sphereMaterial.color=Color.white;
+    }
+    void Update(){
+        if(Input.GetMouseButtonDown(0)){
+            SelectImage();
         }
+    }
+
+    public void SelectImage(){
+        RaycastHit2D hit=Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+        if(hit.collider!=null){
+            Debug.DrawLine(transform.position, hit.point, hit.collider.GetComponent<SpriteRenderer>().color,1f);
+            sphereMaterial.color=hit.collider.GetComponent<SpriteRenderer>().color;
+        }
+    }
+
+    public void Exit(){
+        sphereMaterial.color=Color.white;
     }
 }
